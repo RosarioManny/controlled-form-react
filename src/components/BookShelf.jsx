9886277d09a928e0407316461cc93c135b14ec2e
RosarioManny <rosario.emm47@gmail.com>
2024-10-16 18:12:20 -0400

    Completed

--- src/components/BookShelf.jsx
+++ src/components/BookShelf.jsx
@@ -8,23 +8,53 @@ const BookShelf = () => {
     }])
 
     // This will update the form
-    const handleInputChange = () => {
-
+    const handleInputChange = (evt) => {
+        // Setting the newBook to the information that was inputted.
+        setNewBook({...newBook, [evt.target.name]: evt.target.value})
     }
 
-    const secondInput = () => {
-
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        setBooks([...books, newBook])
+        
+        setNewBook({title: '', author: ''})
     }
+
+    const listedBooks = books.map((book, idx)=>{
+        return (
+        <div className="bookCard "key={idx}>
+           <p><em>{book.title}</em> </p> 
+
+            <p>By <b>{book.author}</b></p>
+        </div>  
+        )
+    })
 
     return (
         <div className="bookshelfDiv">
         <div className="formDiv">
             <h3>Add a Book</h3>
-            <form onSubmit={handleInputChange}>
-                <input></input>
+            <form onSubmit={handleSubmit}>
+                <label>Name:</label>
+                <input
+                id="title"
+                name='title'
+                value={newBook.title}
+                onChange={handleInputChange}
+                />
+                <label>Author:</label>
+                <input 
+                id='author'
+                name='author'
+                value={newBook.author}
+                onChange={handleInputChange}
+                />
+                <button type='submit'>Add Book</button>
             </form>
         </div>
-        <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+        <div className="bookCardsDiv">
+           {listedBooks}
+        </div>
     </div>
     )
 }
